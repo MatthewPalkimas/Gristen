da1641373e7c0e31c2703fd8f763f3111227f9ef
Matthew Palkimas <matthew.palkimas@gmail.com>
2020-04-27 23:50:49 -0400

    Updated some python and added a chat that doesnt work!!!!R#@JLFM#P!OCJPM!O

--- flask_test.py
+++ flask_test.py
@@ -1,78 +1,66 @@
 import flask
 from gristen import Gristen, SongNotFound
+import flask_socketio
 
 app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = flask_socketio.SocketIO(app)
 
 default_song = "Sound & Color"
+
+gs = None
 
 
 @app.route('/audio<song>', methods=["GET", "POST"])
 def audio(song):
     if flask.request.method == "GET":
         try:
-            gs = Gristen(song)
+            global gs
+            if gs is None:
+                gs = Gristen(song)
             return flask.Response(gs.generate_wav_data())
         except SongNotFound:
             raise SongNotFound
 
 
 @app.route('/', methods=["GET", "POST"])
-def index():
+def sessions():
+    global gs
     errors = ""
     if flask.request.method == "POST":
-        song = flask.request.form['song_name']
         try:
-            gs = Gristen(song)
-            song_name = gs.song_name
-            song_artist = gs.song_artist
-            return '''
-                <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                        <title>Gristen</title>
-                    </head>
-                    <body>
-                        <p>Currently listening to</p>
-                        <p>{song_name} by {song_artist}</p>
-                        <audio controls autoplay loop>
-                            <source src="/audio{song}" type="audio/x-wav;codec=pcm">
-                            Your browser does not support the audio element.
-                        </audio>
-                        <p>To change the current song enter below:</p>
-                        <form method="post" action=".">
-                            <p><input name="song_name" /></p>
-                            <p><input type="submit" value="Gristen" /></p>
-                        </form>
-                    </body>
-                </html>
-                '''.format(song=song, song_name=song_name, song_artist=song_artist)
-        except SongNotFound:
-            errors = "<p>{!r} is not available.</p>\n".format(flask.request.form["song_name"])
-    return '''
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Gristen</title>
-        </head>
-        <body>
-            <p>Hello! Enter the song name you want to listen to :)</p>
-            {errors}
-            <form method="post" action=".">
-                <p><input name="song_name" /></p>
-                <p><input type="submit" value="Gristen" /></p>
-            </form>
-            <p>If you want a sample song then click play below!</p>
-            <audio controls>
-                <source src="/audio{song}" type="audio/x-wav;codec=pcm">
-                Your browser does not support the audio element.
-            </audio>
-        </body>
-    </html>
-    '''.format(song=default_song, errors=errors)
+            song = flask.request.form['song_name']
+        except:
+            song = gs.song_name
+            return flask.render_template('test.html', song=song, song_name=gs.song_name, song_artist=gs.song_artist)
+        gs = Gristen(song)
+        return flask.render_template('test.html', song=song, song_name=gs.song_name, song_artist=gs.song_artist, errors=errors)
+    else:
+        return flask.render_template('test.html', song=default_song)
 
 
-app.run(debug=True, port=6464)
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+@socketio.on('song change')
+def handle_song_request(json, methods=['GET', 'POST']):
+    print(json)
+    global gs
+    song = json
+    gs = Gristen(song)
+    song_name = gs.song_name
+    song_artist = gs.song_artist
+    print('received song request: ' + str(json))
+    socketio.emit('')
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=6458)
+# app.run(debug=True, port=6464)
--- gristen.py
+++ gristen.py
@@ -4,8 +4,19 @@ import time
 import os
 import glob
 import sys
+import string
+import youtube_dl
+import youtube_search
+import subprocess
 
 CHUNK = 1024
+youtube_options = {
+    'format': 'bestaudio/best',
+    'extractaudio': True,  # only keep the audio
+    'audioformat': "mp3",  # wav or mp3 hopefully
+    'outtmpl': 'music/%(id)s.mp3',  # name the file the ID of the video
+    'noplaylist': True,  # only download single song, not playlist
+}
 
 
 class SongNotFound(Exception):
@@ -16,32 +27,63 @@ class Gristen:
     def __init__(self, song_requested=None):
         self.music_path = os.path.join(os.getcwd(), 'music')
         self.wf = None
-        if song_requested is not None:
+        self.wav_path = None
+        self.utube_title = None
+        if song_requested is not None or song_requested != '':
             try:
                 self.lil_path = self.find_song(song_requested)
                 self.wav_file = os.path.join(self.music_path, self.lil_path)
+                print("found song %s" % self.wav_file)
             except SongNotFound:
                 raise SongNotFound
-        self.wf = wave.open(os.path.join('music', self.wav_file), 'rb')
-        self.get_song_info()
-        self.generate_wav_data()
+        else:
+            raise SongNotFound
+        self.wf = wave.open(self.wav_file, 'rb')
+        self.get_song_info(song_requested)
+        # self.generate_wav_data()
 
     def find_song(self, song_requested):
         song_db = glob.glob(os.path.join(self.music_path, '*'))
         for song in song_db:
-            song_name = song.split('\\')[-1].split(' - ')[0]
-            if song_requested == song_name:
+            song_name = song.split('\\')[-1]
+            if song_name.lower().__contains__(song_requested.lower()):
                 return song.split('\\')[-1]
-        raise SongNotFound
+        try:
+            print("attempting to download song %s" % song_requested)
+            song_url = youtube_search.YoutubeSearch(song_requested + ' lyrics', max_results=3).to_dict()[0]['link']
+            url = 'https://www.youtube.com' + song_url
+            with youtube_dl.YoutubeDL(youtube_options) as ydl:
+                r = ydl.extract_info(url, download=True)
+                self.utube_title = r['title'].replace('\"', '')
+                self.utube_title = self.utube_title.replace('/', '')
+                song_mp3 = os.path.join(self.music_path, r['id'] + '.mp3')
+                self.wav_path = os.path.join(self.music_path, self.utube_title + '.wav')
+                subprocess.call(['ffmpeg',
+                                 '-n',
+                                 '-i',
+                                 song_mp3,
+                                 self.wav_path])
+                os.remove(song_mp3)
+                return self.wav_path
+        except Exception as e:
+            print(e)
+            raise SongNotFound
 
-    def get_song_info(self):
+    def get_song_info(self, song_requested):
         song_length = self.wf.getnframes()
         self.song_duration = int(song_length / self.wf.getframerate())
         self.minutes = self.song_duration / 60
         self.seconds = self.song_duration % 60
-        song_info = self.wav_file.split('\\')[-1].split('.')[0].split(' - ')
-        self.song_name = song_info[0]
-        self.song_artist = song_info[1]
+        try:
+            song_info = self.wav_file.split('\\')[-1].split('.')[0].split(' - ')
+            self.song_name = song_info[1]
+            self.song_artist = song_info[0]
+        except:
+            if self.utube_title is not None:
+                self.song_name = self.utube_title
+            else:
+                self.song_name = self.wav_file.split('\\')[-1].split('.')[0]
+            self.song_artist = ""
 
     def callback(self, in_data, frame_count, time_info, status):
         wav_bytes = self.wf.readframes(frame_count)
@@ -53,11 +95,6 @@ class Gristen:
                         channels=self.wf.getnchannels(),
                         rate=self.wf.getframerate(),
                         output=True)
-        # self.stream = p.open(format=p.get_format_from_width(self.wf.getsampwidth()),
-        #                 channels=self.wf.getnchannels(),
-        #                 rate=self.wf.getframerate(),
-        #                 output=True,
-        #                 stream_callback=self.callback)
 
         print("Current Song:\n--- %s by %s" % (self.song_name, self.song_artist))
         with open(self.wav_file, "rb") as fwav:
@@ -65,7 +102,26 @@ class Gristen:
             while data:
                 yield data
                 data = fwav.read(1024)
+        # self.stream = p.open(format=p.get_format_from_width(self.wf.getsampwidth()),
+        #                 channels=self.wf.getnchannels(),
+        #                 rate=self.wf.getframerate(),
+        #                 output=True,
+        #                 stream_callback=self.callback)
+        #
+        # self.stream.start_stream()
+        # for second_count in range(self.song_duration):
+        #     total_of_song = round(second_count / self.song_duration * 60)
+        #     timestamp = ('▮' * total_of_song) + ('▯' * (60 - total_of_song))
+        #     sys.stdout.write("\r")
+        #     sys.stdout.write("--- %02d:%02d %s %02d:%02d" %
+        #                      (second_count / 60, second_count % 60, timestamp, self.minutes, self.seconds))
+        #     sys.stdout.flush()
+        #     time.sleep(1)
+        #
+        # while self.stream.is_active():
+        #     time.sleep(0.1)
 
+        print("\nstopping stream")
         self.stream.stop_stream()
         self.stream.close()
 
